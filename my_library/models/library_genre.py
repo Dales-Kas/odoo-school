28c2713bc4a6d063e58620c0cdf50b43b6166b7a
Dales Kas <102468134+Dales-Kas@users.noreply.github.com>
2023-07-11 14:24:45 +0300

    Delete my_library directory

--- my_library/models/library_genre.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class LibraryGenre(models.Model):
-    _name = 'library.genre'
-    _description = 'Library Book Genres'
-
-    name = fields.Char(required=True)
--- my_library/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    telegram = fields.Char(
-        string='Telegram Nickname',
-    )
